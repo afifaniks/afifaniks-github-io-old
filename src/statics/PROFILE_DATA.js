f6494c5bd95403de970ae7670c24598033f405cc
afif-mamun <afifanik@gmail.com>
2022-01-10 11:21:38 +0600

    Information updated

--- src/statics/PROFILE_DATA.js
+++ src/statics/PROFILE_DATA.js
@@ -1,12 +1,13 @@
 const PROFILE_DATA = Object.freeze({
     profileName: "Afif Al Mamun",
-    headline: "Machine Learning Engineer",
+    headline: "Machine Learning Engineer | Gold Medalist",
     bio: "Afif Al Mamun is a software engineer from Dhaka, Bangladesh. "
         + "He is currently working at [Infolytx Bangladesh Ltd.](infolytx.com) as a software engineer in machine learning. "
         + "Previously, he used to work at [ServicEngine Ltd.](sebpo.com) as a software engineer where his main role was to develop web scrapers as per clients' requirements and also developing modules in Java for the company framework. "
         + "Being an AI enthusiast, he is actively researching in the field of Deep Learning & Computer Vision and has published a few scholarly articles. "
         + "Afif obtained his bachelor degree from the Department of Computer Science and Engineering of [Daffodil International University](https://daffodilvarsity.edu.bd/). "
-        + "During his study in the university, he used to be a student prefect and took different courses.",
+        + "He received the \"50 Years Victory of Bangladesh Gold Medal\" on the [9th convocation](https://convocation.daffodilvarsity.edu.bd/index.php/convocation/nineth-convocation) of the university held in 2022."
+        + "During his study in the university, he collaborated with instructors as a student prefect.",
     homepage: "https://afifaniks.github.io",
     email: "afifanik@gmail.com",
     socialLinks: {
@@ -51,6 +52,12 @@ const PROFILE_DATA = Object.freeze({
         },
     ],
     achievements: [
+        {
+            title: "50 Years Victory of Bangladesh Gold Medal",
+            time: "2022",
+            awardedBy: "Daffodil International University",
+            desc: "Recognition of academic accomplishment during the undergrad."
+        },
         {
             title: "Professor Dr.  M. Lutfar Rahman Scholarship",
             time: "2017",
